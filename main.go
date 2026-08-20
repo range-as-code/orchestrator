3c411699e0eaddc92aa2e09cc338fd5120414a44
Khai Phung <phunga.prod@gmail.com>
2026-08-20 04:23:34 +0000

    test code

--- main.go
+++ main.go
@@ -22,4 +22,39 @@ func main() {
 		log.Fatalf("authentication failed: %v", err)
 	}
 	fmt.Println("token:", client.Token)
+
+	fmt.Println("using token to create connection...")
+	fmt.Println("creating connection group")
+
+	groupID, err := client.CreateConnectionGroup(guac.ConnectionGroupSpec{
+		ParentIdentifier: "ROOT",
+		Name:             "Group A",
+		Type:             "ORGANIZATIONAL",
+	})
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	fmt.Println("Group created:", groupID)
+
+	got, err := client.CreateConnection(guac.ConnectionSpec{
+		ParentIdentifier: groupID,
+		Name:             "test box Hello",
+		Protocol:         "ssh",
+		Hostname:         "127.112.0.0",
+		Port:             "22",
+		Username:         "changeme",
+		Password:         "changeme",
+	})
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	fmt.Println("connection id returned: ", got)
+
+	// err = client.DeleteConnection(got)
+	// if err != nil {
+	// 	log.Fatalf("delete connection: %v", err)
+	// }
+
 }
